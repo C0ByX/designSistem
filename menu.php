e24c87156f34cf75ea6823e8881261e5794b1cd8
Evgen <byzhehek@gmail.com>
2023-04-25 20:51:27 +0300

    FirstCommit

---
+++ menu.php
@@ -0,0 +1,44 @@
+<section class="menu">
+        <h1>Bonfire дизайн система для email-рассылок</h1>
+        <ul>
+            <li class="listTitle">
+                <a href="#">Введение</a>
+                <ul class="listElem">
+                    <li>
+                        <a href="#">Состав дизайн системы</a>
+                    </li>
+                    <li>
+                        <a href="#">Цель</a>
+                    </li>
+                    <li>
+                        <a href="#">Как пользоваться этим гайдом</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="listTitle">
+                <a href="#">Платформа</a>
+                <ul class="listElem">
+                    <li>
+                        <a href="#">Unisender и его возможности</a>
+                    </li>
+                    <li>
+                        <a href="#">Логин и пароль</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="listTitle">
+                <a href="#">Мастер шаблон для регулярной рассылки</a>
+                <ul class="listElem">
+                    <li>
+                        <a href="#">Требования</a>
+                    </li>
+                    <li>
+                        <a href="#">Как пользоваться</a>
+                    </li>
+                    <li>
+                       <a href="#">Изменяемые блоки</a> 
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </section>
